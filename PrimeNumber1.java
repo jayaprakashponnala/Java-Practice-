6d085506fe0f806a22e1268092770d7683f82dd2
jayaprakashponnala <jayaprakashponnala13@gmail.com>
2024-05-28 02:52:50 +0530

    Prime NUmber code added

---
+++ PrimeNumber1.java
@@ -0,0 +1,17 @@
+//  Prime Number :  
+
+public class PrimeNumber1{
+    public static void main(String[] args) {
+        int i, count = 0, numb = 5;
+        for (i = 1; i <= numb; i++) {
+            if (numb % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            System.out.println(numb + " is Prime");
+        } else {
+            System.out.println(numb + " is not Prime");
+        }
+    }
+}
