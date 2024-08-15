98534e6209ecb4813a638b24efb651e6c768c4ce
Jayaprakash Ponnala <125442261+jayaprakashponnala@users.noreply.github.com>
2024-08-16 00:54:54 +0530

    Add files via upload

---
+++ PyramidNumbers.java
@@ -0,0 +1,17 @@
+
+// PyramidNumbers
+
+class PyramidNumbers {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(" "); 
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(" "+i);
+            }
+            System.out.println();
+        }
+        
+     }
+}
